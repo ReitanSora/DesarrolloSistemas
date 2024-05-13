1003d5e72245ce4f03953ea5e7b50e113c7bb628
Stiven Pilca <stivenpilca@gmail.com>
2024-05-12 19:15:00 -0500

    fix: index file

--- index.py
+++ index.py
@@ -1,9 +1,13 @@
 from config import config
 from src import init_app, page_not_found
 
-configuration = config['development']
-app = init_app(configuration)
+def test():
+    configuration = config['development']
+    app = init_app(configuration)
+
+    return app
 
 if __name__ == '__main__':
+    app = test()
     app.register_error_handler(404, page_not_found)
     app.run()
