e65d079833244a3c4f885d52f24354a1f38e2665
Stiven Pilca <stivenpilca@gmail.com>
2024-05-12 19:22:58 -0500

    fix: config file

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ from src.database.db_connection import get_connection
 
 class DevelopmentConfig():
     DEBUG = True
-    SERVER_NAME = '127.0.0.1:4321'
     MYSQL_HOST = ''.join(get_connection()[0])
     MYSQL_PORT = int(''.join(get_connection()[1]))
     MYSQL_USER = ''.join(get_connection()[2])
